269a12ffc4eadf6c613ff54f34be4e695426f634
Estefany Valdivieso Chávez <Estefany Valdivieso Chvez>
2017-05-15 21:17:28 -0500

    fetch example

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,16 @@
+import { combineEpics } from 'redux-observable'
 import { combineReducers } from 'redux'
 
 import counter from '../ducks/counter'
+import users, { fetchUserEpic } from '../ducks/users'
+
+export const rootEpic = combineEpics(
+  fetchUserEpic
+);
 
 const rootReducer = combineReducers({
-  counter
+  counter,
+  users
 })
 
 export default rootReducer
